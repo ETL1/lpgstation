e90e330117e91fee73743451ab330f862f2f9da4
ro07 <roo7adm1@gmail.com>
2025-10-08 15:48:23 +0200

    Setting Update

--- lpg_station/settings.py
+++ lpg_station/settings.py
@@ -98,7 +98,7 @@ STATICFILES_DIRS = [
 ]
 
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'static')
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
